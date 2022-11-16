213aa242570f8d41e82432c01ae7ed8f32f49b20
Moe-Zart <mohamedzreika03@outlook.com>
2022-11-17 00:55:17 +1100

    nav + header commit

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import nav_logo from "../assets/Logo.png";
+import nav_wave from "../assets/Nav-Wave.png";
+import nav_circle from "../assets/Logo Circle.png";
 
 const Nav = () => {
-    return (
-        <div>
-            Nav Section
-        </div>
-    );
-}
+  return (
+    <div className="nav">
+      <img className="nav_wave" src={nav_wave} alt="" />
+      <div className="nav_logo-wrapper">
+        <img className="nav_logo" src={nav_logo} alt="" />
+        <img className="nav_circle" src={nav_circle} alt="" />
+      </div>
+      <ul className="nav_links">
+        <li>
+          <Link to="/" className="nav_link">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="nav_link">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="nav_link">
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="nav_link">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Nav;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,12 @@ const Home = () => {
     return (
         <div>
             Home Page
+            <h1 className="header_name header_first-name">MOHAMED</h1>
+            <div className="header_content-center">
+                <h1 className="header_name">ZREIKA</h1>
+                <h3 className="header_role">Web Designer / Developer</h3>
+                <p className="header_description">A design oriented frontend developer <br/>residing in Sydney, NSW</p>
+            </div>
         </div>
     );
 }
